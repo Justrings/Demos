ab396adea7ea65176d00b76efccb773d02a576e3
coliver <370928511@qq.com>
2017-10-14 01:34:55 +0800

    update -- half star select

--- 2017-10-scoreWithStar/js/main.js
+++ 2017-10-scoreWithStar/js/main.js
@@ -1,92 +1,81 @@
-var mark = (function(){
 
-	var init = function(num, selector) {
-		var $rating = $(selector),
-		$items = $rating.find('.ratingItem');
-		
-		markOn(num, $items);
-
-		console.log([$rating]);
-		$rating.on('mouseover', '.ratingItem', function() {
-			markOn($(this).index(), $items);
-		}).on('click', '.ratingItem', function() {
-			num = $(this).index();
-		}).on('mouseout', function() {
-			markOn(num, $items);
+function markOn($rating, options){
+	var deafultSetting = {
+		n: 3,
+		readOnly: false,
+		half: true,
+		onceSign: true,
+	};
+	this.options = $.extend({}, deafultSetting, options);
+	this.$rating = $rating;
+	this.n = options.n;
+	this.$rateItems = this.$rating.find('.ratingItem');
+	this.len = this.$rateItems.length;
+}
+markOn.prototype.lightOn = function ($rateItems, n) {
+	if(this.options.half){
+		var m = Math.floor(n) - 0;
+		n = n - Math.floor(n);
+		console.log(['m:'+m,'n:'+n]);
+		$rateItems.each(function(index) {
+			if(index <= m){
+				console.log('000000');
+				if(index < m){
+					$(this).addClass('marked').removeClass('halfMarked');
+				}else{
+					if(n<0.5&&n>0){
+						$(this).removeClass('marked').addClass('halfMarked');
+					}else{
+						$(this).removeClass('halfMarked').addClass('marked');
+					}
+				}
+			}else{
+				$(this).removeClass('marked').removeClass('halfMarked');
+			}
 		});
-	};
-
-	var markOn = function(num,$items) {
-		 $items.each(function(index) {
-	 		if(index <= num){
-	 			$(this).addClass('marked');
-	 		}else{
-	 			$(this).removeClass('marked');
-	 		}
-		 });
-		};
-
-	return {
-		init: init
-	};
-
-})();
-
-mark.init(2,'.rating');
-mark.init(3,'.rating2');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var rateContainer = document.getElementsByClassName('rating')[0],
-	rateItems = rateContainer.children;
-	flag = false;
-
-var getItem = function(target) {
-	for(var i = 0; i < rateItems.length; i++){
-		if(target === rateItems[i])
-			return i;
+	}else{
+		$rateItems.each(function(index) {
+			if(index <= n){
+				$(this).addClass('marked');
+			}else{
+				$(this).removeClass('marked');
+			}
+		});
 	}
 };
-var handler = function(e) {
-	var target = e.target;
-		if(target.tagName.toLowerCase() !== 'li'){
-			return;
-		}else if(flag){
-			return;
-		}
-	var i = getItem(target);
-		
-	switch(e.type) {
-		case 'click':
-			console.log('您已成功评分!!!');
-			flag = true; 
-			break;
-		case 'mouseover':
-			for(var j = 0; j < rateItems.length; j++){
-				if(j <= i){
-					rateItems[j].classList.add('marked');
-					console.log(j);
-				}else{
-					rateItems[j].classList.remove('marked');
+markOn.prototype.port = function(){
+		var self = this;
+		var indexHalf = 0;
+		this.lightOn(this.$rateItems, this.n);
+		this.$rating.on('mousemove', '.ratingItem', function(event) {
+				if(self.options.half){
+					indexHalf = (event.pageX-$(this).offset().left+1)/32;
 				}
-			}
-			break;
-	}
-} ;
+			self.lightOn(self.$rateItems, $(this).index()+indexHalf);
+			 }).on('click', '.ratingItem', function(event) {
+				if(self.options.readOnly){
+					console.log("readOnly");
+					return;
+				}
+				if(self.options.onceSign){
+					self.options.onceSign = false;
+					self.lightOn(self.$rateItems, $(this).index()+indexHalf);
+					self.n = $(this).index()+indexHalf;
+				}else{
+					console.log("all reversed");
+				}
+			 }).on('mouseout', '.ratingItem', function(event) {
+			 	console.log(indexHalf);
+				self.lightOn(self.$rateItems, self.n);
+			 });
+	};
+markOn.prototype.init = function(){
+	var self = this;
+	this.port(this.$rating, this.n);
+};
 
-rateContainer.addEventListener('mouseover',handler);
-rateContainer.addEventListener('click', handler);
-rateContainer.addEventListener('mouseout', handler);*/
+var mark = new markOn($(".rating"),{n: 3,});
+mark.init();
+
+var mark2 = new markOn($(".rating2"),{n: 2,})
+mark2.init();
